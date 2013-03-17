95ce09f3b35757ee34d5865e59f768dff4889b2f
Jakob G. Thomsen <jakob@gedefar.dk>
2013-03-17 21:51:48 +0100

    Undid all the shit that WebStorm did on the last commit. And made the 2d context a field in the GraphPainter class.

--- graph-painter.dart
+++ graph-painter.dart
@@ -1,33 +1,26 @@
 /**
 * Class for painting a graph to a HTML canvas.
 */
-
 library graph_gui;
-
 import "dart:html" hide Node;
-
 import "dart:math";
-
 import 'dajkstra/dajkstra.dart';
 
 class GraphPainter {
   CanvasElement _canvasElement;
-
   CanvasRenderingContext2D _context;
-
   num _xmax;
-
   num _ymax;
-
   num _mapWidth;
-
   num _mapHeight;
-
   num _cellWidth;
-
   num _cellHeight;
 
-  GraphPainter(this._canvasElement, this._xmax, this._ymax, num mapWidthMax, num mapHeightMax) {
+  GraphPainter(this._canvasElement,
+               this._xmax,
+               this._ymax,
+               num mapWidthMax,
+               num mapHeightMax) {
     _cellWidth = (mapWidthMax / _xmax).floor();
     _cellHeight = (mapHeightMax / _ymax).floor();
     _mapWidth = _cellWidth * _xmax;
@@ -38,79 +31,83 @@ class GraphPainter {
   }
 
   void _initCanvas() {
-    context.clearRect(0, 0, _mapWidth, _mapHeight);
-    _drawGrid(context);
+    _context.clearRect(0, 0, _mapWidth, _mapHeight);
+    _drawGrid();
   }
 
   void drawGraph(DisplayableGraph graph) {
     _initCanvas();
-    _drawEdges(context, graph);
-    _drawNodes(context, graph);
+    _drawEdges(graph);
+    _drawNodes(graph);
   }
 
   void drawPath(DisplayableGraph graph, PList<Node> path) {
     _initCanvas();
-    _drawEdges(context, graph);
-    _drawPath(context, graph, path);
-// Nodes that are in the path are colored white. All other gray (except start and end)_drawNodes(context, graph, colorFun: (Node n) => (path.any((Node other) => n.id == other.id)) ? "white" : "gray");
+    _drawEdges(graph);
+    _drawPath(graph, path);
+    // Nodes that are in the path are colored white. All other gray (except start and end)
+    _drawNodes(graph, colorFun: (Node n) =>
+        (path.any((Node other) => n.id == other.id))? "white": "gray");
   }
 
-  void _drawPath(CanvasRenderingContext2D context, DisplayableGraph graph, PList<Node> path) {
+  void _drawPath(DisplayableGraph graph, PList<Node> path) {
     if (path.empty) return;
-    context.beginPath();
-    context.strokeStyle = "lightgreen";
-    context.lineWidth = 5;
+    _context.beginPath();
+    _context.strokeStyle = "lightgreen";
+    _context.lineWidth = 5;
     path.foldr((node, acc) {
       var eucNode = graph.euclidNodeFromId(node.id);
       if (acc) {
-        context.moveTo(_transformX(eucNode.x), _transformY(eucNode.y));
+        _context.moveTo(_transformX(eucNode.x), _transformY(eucNode.y));
       } else {
-        context.lineTo(_transformX(eucNode.x), _transformY(eucNode.y));
+        _context.lineTo(_transformX(eucNode.x), _transformY(eucNode.y));
       }
       return false;
-    }, true);
-// Acc states whether it is the firstcontext.stroke();
+    }, true); // Acc states whether it is the first
+    _context.stroke();
   }
 
-  void _drawGrid(CanvasRenderingContext2D context) {
-    context.beginPath();
-    context.strokeStyle = "#eee";
-    for (int i = _cellWidth / 2; i < _mapWidth; i += _cellWidth) {
-      context.moveTo(i, 0);
-      context.lineTo(i, _mapHeight);
+  void _drawGrid() {
+    _context.beginPath();
+    _context.strokeStyle = "#eee";
+    for (int i = _cellWidth/2; i < _mapWidth; i += _cellWidth) {
+      _context.moveTo(i, 0);
+      _context.lineTo(i, _mapHeight);
     }
-    for (int i = _cellHeight / 2; i < _mapHeight; i += _cellHeight) {
-      context.moveTo(0, i);
-      context.lineTo(_mapWidth, i);
+    for (int i = _cellHeight/2; i < _mapHeight; i += _cellHeight) {
+      _context.moveTo(0, i);
+      _context.lineTo(_mapWidth, i);
     }
-    context.stroke();
+    _context.stroke();
   }
 
-  num _transformX(num x) => x * _cellWidth + _cellWidth / 2;
+  num _transformX(num x) => x * _cellWidth + _cellWidth/2;
+  num _transformY(num y) => y * _cellHeight + _cellHeight/2;
 
-  num _transformY(num y) => y * _cellHeight + _cellHeight / 2;
-
-  void _drawNodes(CanvasRenderingContext2D ctx, DisplayableGraph graph, {
-  colorFun: null
-  }) {
-    colorFun = (colorFun == null) ? (_) => "gray" : colorFun;
+  void _drawNodes(DisplayableGraph graph, {colorFun: null}) {
+    colorFun = (colorFun == null)? (_) => "gray" : colorFun;
     PList<Node> nodes = graph.graph.nodes;
-    while (!nodes.empty) {
+    while(!nodes.empty) {
       Node node = nodes.hd;
       EucNode eucNode = graph.euclidNodeFromId(node.id);
-      ctx.beginPath();
+      _context.beginPath();
       var sx = _transformX(eucNode.x);
       var sy = _transformY(eucNode.y);
-      ctx.arc(sx, sy, 17, 0, PI * 2, true);
-      ctx.closePath();
-      ctx.strokeStyle = "gray";
-      ctx.lineWidth = 2;
-      ctx.fillStyle = (node.id == 0) ? "lightgreen" // Start node : (node.id == graph.graph.nodeCount - 1) ? "#F17022" //end node : colorFun(node);ctx.fill();ctx.stroke();
+      _context.arc(sx, sy, 17, 0, PI*2, true);
+      _context.closePath();
+      _context.strokeStyle = "gray";
+      _context.lineWidth = 2;
+      _context.fillStyle
+         = (node.id == 0) ? "lightgreen" // Start node
+          : (node.id == graph.graph.nodeCount - 1) ? "#F17022" //end node
+          : colorFun(node);
+      _context.fill();
+      _context.stroke();
       nodes = nodes.tl;
     }
   }
 
-  void _drawEdges(CanvasRenderingContext2D ctx, DisplayableGraph graph) {
+  void _drawEdges(DisplayableGraph graph) {
     graph.graph.nodes.map((Node srcNode) {
       EucNode eucNode = graph.euclidNodeFromId(srcNode.id);
       var eucNodeSX = _transformX(eucNode.x);
@@ -118,15 +115,15 @@ class GraphPainter {
       PList<Edge<Node>> edges = graph.graph.adjacent(srcNode);
       edges.map((Edge<Node> edge) {
         var dstNode = edge.dest;
-        ctx.beginPath();
-        ctx.strokeStyle = "gray";
-        ctx.lineWidth = 2;
+        _context.beginPath();
+        _context.strokeStyle = "gray";
+        _context.lineWidth = 2;
         EucNode dstEucNode = graph.euclidNodeFromId(dstNode.id);
         var dstNodeSX = _transformX(dstEucNode.x);
         var dstNodeSY = _transformY(dstEucNode.y);
-        ctx.moveTo(eucNodeSX, eucNodeSY);
-        ctx.lineTo(dstNodeSX, dstNodeSY);
-        ctx.stroke();
+        _context.moveTo(eucNodeSX, eucNodeSY);
+        _context.lineTo(dstNodeSX, dstNodeSY);
+        _context.stroke();
       });
     });
   }
